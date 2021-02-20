1b7d25312cdba25604b591a39cd73c8a1a58bee7
Maria Sitkovets <m.sitkovets@gmail.com>
2021-02-20 16:32:27 -0500

    Create first components in onboarding

--- brickhacks/src/App.js
+++ brickhacks/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, {Fragment, useState, useEffect} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+// components
+import LandingPage from './components/LandingPage';
+import ProfilePage from './components/ProfilePage';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
+  const user = null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <div className="container">
+        {user ?
+            <ProfilePage />
+          :
+          <Switch>
+            {/* <Route exact path="/signup" render={ 
+              <SignUp /> 
+            }/> */}
+            <Route exact path="/">
+              <LandingPage /> 
+            </Route>
+            <Route exact path="/signup">
+              <SignUp /> 
+            </Route>
+            <Route exact path="/signup">
+              <SignIn /> 
+            </Route>
+          </Switch>
+        }
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ brickhacks/src/components/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+   <p>landing page</p>
+  ) 
+};
+export default LandingPage;
