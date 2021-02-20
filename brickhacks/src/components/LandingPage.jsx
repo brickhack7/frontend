0da1be16a272a2574b76b0f6164cf26d81122ca0
Maria Sitkovets <m.sitkovets@gmail.com>
2021-02-20 18:08:13 -0500

    Add button to LandingPage

--- brickhacks/src/components/LandingPage.jsx
+++ brickhacks/src/components/LandingPage.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import { Redirect } from 'react-router-dom';
 
 const LandingPage = () => {
+  const [toSignUp, setToSignUp] = useState(false);
   return (
-   <p>landing page</p>
+    <>
+      {toSignUp ? <Redirect to="/signup" /> : null}
+      <h1>Rendezvous</h1>
+      <h2>Go on a new adventure with someone new</h2>
+      <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => setToSignUp(true)}>Sign Up Now</button>
+    </>
   ) 
 };
 export default LandingPage;
